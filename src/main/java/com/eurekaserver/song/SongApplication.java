0b055da78e25bab6511c3117fe3d8a95f58a5a33
vasilijpetecuk <frostpv@gmail.com>
2023-06-10 12:55:33 +0300

    [BE][TSK-5] - basic config for eureka server

--- src/main/java/com/eurekaserver/song/SongApplication.java
+++ src/main/java/com/eurekaserver/song/SongApplication.java
@@ -2,8 +2,10 @@ package com.eurekaserver.song;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SongApplication {
 
 	public static void main(String[] args) {
